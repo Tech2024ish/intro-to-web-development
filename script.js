f1273ffbdc0ab2f26fb034e52d4709b9d73ea980
Tech2024ish <ishimwejeanclaude88@gmail.com>
2025-05-02 19:05:49 +0000

    tech@website

---
+++ script.js
@@ -0,0 +1,8 @@
+var OriginalTilte=document.title;
+
+//this is a function for changint the title
+function changeTitle(newTitle){
+    document.title=newTitle;
+}
+let newTitle="This is the GYM";
+changeTitle(newTitle);
