1e39fda4083a861e0af4dcb8c896e1a9715a29b6
Tech2024ish <ishimwejeanclaude88@gmail.com>
2025-05-04 09:39:57 +0000

    first session covered

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ var myText=document.createTextNode("This is Harambee Morning Course");
 //adding the content i careated to my html document
 myElement.appendChild(myText);
 navBar.appendChild(myElement);
+
+//styling the image by using JavaSript
+document.getElementById("welcome").style.fontSize="2em";
+
+//creating the function for changing the backgaound
+function changeBackgroundColor(color){
+    let section=document.getElementById("codding-journey");
+    section.style.backgroundColor="color";
+}
+document.getElementById("intro").addEventListener('click', () => backgroundColor("#2CA02C"));
+document.getElementById("react").addEventListener('click', () => backgroundColor("#f4f4f4"));
+document.getElementById("bacend").addEventListener('click', () => backgroundColor("rgb(0, 0, 228)"));
