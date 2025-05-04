eda9dc2408f70402ab0bf04d5c5ace52bf57e619
Tech2024ish <ishimwejeanclaude88@gmail.com>
2025-05-04 08:00:25 +0000

    Good morning

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function changeTitle(newTitle){
 }
 let newTitle="This is the GYM";
 changeTitle(newTitle);
+
+var navBar=document.getElementById("myNav");
+var myElement=document.createElement("h1");
+var myText=document.createTextNode("This is Harambee Morning Course");
+
+//adding the content i careated to my html document
+myElement.appendChild(myText);
+navBar.appendChild(myElement);
